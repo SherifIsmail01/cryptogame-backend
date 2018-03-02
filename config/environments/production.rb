bbb342cc693e0c224e0047b6490995970be50f15
Sherif Ismail <sherifismail01@gmail.com>
2018-03-01 19:51:36 -0800

    cors issue6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://crypto-game.herokuapp.com/'
+      origins '*'
       resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
     end
   end 
