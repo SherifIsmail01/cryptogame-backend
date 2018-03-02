f807935e62f95abd81521e492ec0b89d123c2065
Sherif Ismail <sherifismail01@gmail.com>
2018-03-01 18:11:46 -0800

    cores issue2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'https://crypto-game.herokuapp.com/'
-      resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
+      resource 'https://crypto-game-backend.herokuapp.com/', :headers => :any, :methods => [:get, :post, :put, :options]
     end
   end 
 end
